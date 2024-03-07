dbf160d837354656f91c81abf4474a36ecafbcc5
HAdiiiir <hader.jaamal2010@gmail.com>
2024-03-06 19:43:36 -0800

    a

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+from models.base_model import BaseModel
+"""
+Module class: Amenity
+"""
+
+class Amenity(BaseModel):
+    """definition for class Amenity"""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ constructor method """
+        super().__init__(self, *args, ** kwargs)
