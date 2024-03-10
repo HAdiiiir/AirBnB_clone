88af168c14376acfdf9c56c36dce2b8cb4213c7b
HAdiiiir <hader.jaamal2010@gmail.com>
2024-03-10 11:21:42 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,78 @@
 #!/usr/bin/python3
 """
-Module: base.py
+Module base_model
+Contains a Class that defines all common attributes or
+methods for other classes
 """
-import models
-import uuid
+from uuid import uuid4
 from datetime import datetime
+from models import storage
+import uuid
+import json
+import sys
+import os.path
 
 
 class BaseModel():
-    """
-    Base class which defines all common
-    attributes/methods for other classes
-    """
+    ''' a base class for other classes '''
 
     def __init__(self, *args, **kwargs):
-        """
-        instatiates an object with it's
-        attributes
-        """
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        models.storage.new(self)
+        '''
+        initializes the values
+        '''
+        if kwargs:
+            dtf = '%Y-%m-%dT%H:%M:%S.%f'
+            k_dict = kwargs.copy()
+            del k_dict["__class__"]
+            for key in k_dict:
+                if ("created_at" == key or "updated_at" == key):
+                    k_dict[key] = datetime.strptime(k_dict[key], dtf)
+            self.__dict__ = k_dict
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """
-        Returns the string representation
-        of the instance
-        """
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+        '''
+        print in "[<class name>] (<self.id>) <self.__dict__>" format
+        '''
+        return ('[{}] ({}) {}'.format(
+            self.__class__.__name__,
+            self.id,
+            self.__class__.__dict__))
 
     def save(self):
-        """
-        updates the public instance attribute
-        updated_at with the current datetime
-        """
+        '''
+        updates the public instance attribute updated_at
+        with the current datetime
+        '''
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
+        '''
+        returns a dictionary containing all keysvalues
         of __dict__ of the instance
-        """
-        dict = {**self.__dict__}
-        dict['__class__'] = type(self).__name__
-        dict['created_at'] = dict['created_at'].isoformat()
-        dict['updated_at'] = dict['updated_at'].isoformat()
+        '''
+        dic = {}
+        dic["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, (datetime, )):
+                dic[k] = v.isoformat()
+            else:
+                dic[k] = v
+        return dic
 
-        return dict
+    def to_json(self):
+        '''
+        returns a json containing all keysvalues
+        of __dict__ of the instance
+        '''
+        my_json = self.__dict__.copy()
+        my_json.update({'created_at': self.created_at.strftime(self.dtf)})
+        my_json.update({'__class__': str(self.__class__.__name__)})
+        if hasattr(self, 'updated_at'):
+            my_json.update({'updated_at': self.updated_at.strftime(self.dtf)})
+        return my_json
