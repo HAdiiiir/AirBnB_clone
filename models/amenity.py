4cabb2ebdbff047b7dceba63b73313cf1f0b5729
HAdiiiir <hader.jaamal2010@gmail.com>
2024-03-10 13:05:52 -0700

    a

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,12 @@
-#!/user/bin/python3
+#!/usr/bin/python3
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
-"""
-Module class: Amenity
-"""
+
 
 class Amenity(BaseModel):
-    """definition for class Amenity"""
-    name = ""
+    """Represent an amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
 
-    def __init__(self, *args, **kwargs):
-        """ constructor method """
-        super().__init__(self, *args, ** kwargs)
+    name = ""
